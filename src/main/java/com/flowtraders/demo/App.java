f69e69ac40f3fc5aa6206c47764a85a9c1558ce9
ioikka <belgampaul@gmail.com>
2023-04-06 00:10:23 +0200

    [FLYWAY-6] Java code migration Example.
    Run main()

--- src/main/java/com/flowtraders/demo/App.java
+++ src/main/java/com/flowtraders/demo/App.java
@@ -1,13 +1,23 @@
 package com.flowtraders.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
 
 import java.time.LocalDateTime;
 
 @Slf4j
 public class App {
+
     public static void main(String[] args) {
         log.info("App starting at {}", LocalDateTime.now());
+        migrateDatabase();
         log.info("Exiting main() at {}", LocalDateTime.now());
+    }
+
+    private static void migrateDatabase() {
+        Flyway flyway = Flyway.configure()
+                .dataSource("jdbc:mariadb://localhost:3306/flyway_demo", "flyway_svc", "PASSWORD")
+                .load();
+        flyway.migrate();
     }
 }
